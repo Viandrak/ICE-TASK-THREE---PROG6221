0fe200e80c916bacaf7db8dbce79e024b33ef058
ST10445089 <st10445089@vcconnect.edu.za>
2025-03-13 14:58:50 +0200

    Shape Class

---
+++ ICE TASK THREE - PROG6221/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_TASK_THREE___PROG6221
+{
+    class Shape
+    {
+        public string Name { get; set; }
+
+        public Shape(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"Shape: {Name}");
+        }
+    }
+}
