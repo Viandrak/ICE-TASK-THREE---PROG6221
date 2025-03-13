54660f254da450036446c4314ecfb0919d03ebeb
ST10445089 <st10445089@vcconnect.edu.za>
2025-03-13 14:55:50 +0200

    Add project files.

---
+++ ICE TASK THREE - PROG6221/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ICE_TASK_THREE___PROG6221
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
