d6bfb36905b25e4f76e2b95da1d93a71fc21a150
ST10445089 <st10445089@vcconnect.edu.za>
2025-03-13 15:00:24 +0200

    Rectangle Class

---
+++ ICE TASK THREE - PROG6221/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_TASK_THREE___PROG6221
+{
+    class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(string name, double width, double height) : base(name)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+}
+}
